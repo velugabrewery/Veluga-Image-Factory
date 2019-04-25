6e6bdf3c2308c4ef03f082ddd063dc8a7c006e70
seongj1n <seongjin@veluga.kr>
2019-04-25 18:02:36 +0900

    changeColor 함수 위치 변경

--- js/script.js
+++ js/script.js
@@ -19,8 +19,6 @@ function colorChange(){
 };
 
 window.onload = function() {
-    colorChange();
-    
     let params = window.location.search.substr(1).split('&');
     for(let i in params) {
         let keyValue = params[i].split('=')
@@ -125,6 +123,7 @@ function beerSubmit(){
     resetOGFontSize();
     changeDefaultFontT1();
     changeDefaultFontT2();
+    colorChange();
 };
 
 function changeYearRound(){
